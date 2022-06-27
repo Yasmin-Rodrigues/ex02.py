9ff21726de58cd8ce0a174d7d2152beb122ff0eb
Yasmin Rodrigues <yasminrodsil@gmail.com>
2022-06-27 15:04:05 -0300

    first commit

---
+++ Untitled-1.py
@@ -0,0 +1,4 @@
+#Faça um programa que leia um número inteiro e mostre na tela o seu sucessor e o seu antecessor.
+n =int(input('Digite um número inteiro:'))
+print('Seu antecessor é:{}'.format(n-1))
+print('Seu sucessor é:{}' .format(n+1))
